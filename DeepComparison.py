817152e0d17180385446858f8cdc6edc34d00905
daijie <daijie@kuaikuaidai.com>
2018-02-06 15:38:16 +0800

    f

--- DeepComparison.py
+++ DeepComparison.py
@@ -6,8 +6,123 @@ import numpy as np
 import tensorflow.contrib.slim as slim
 #import input_data
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from matplotlib import cm
+import itertools
+import numpy as np
+from tensorflow.contrib import learn
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib import layers
+from sklearn import datasets, metrics, preprocessing
+from sklearn import model_selection
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Dense, Flatten
+from keras.models import Model
+from keras.datasets import cifar10
+from keras import utils
+from keras.datasets import mnist
+import sys
+from util.minst_input_data import read_data_sets
 
 
+
+boston = datasets.load_boston()
+x_data = preprocessing.StandardScaler().fit_transform(boston.data)
+y_data = boston.target
+y_data = y_data.reshape(y_data.shape + (1,))
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x_data, y_data, test_size=0.2, random_state=0)
+
+"""
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    
+    #This function prints and plots the confusion matrix.
+    #Normalization can be applied by setting `normalize=True`.
+    
+    plt.imshow(cm, interpolation='nearest', cmap=cmap,aspect='auto')
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+#tf.logging.set_verbosity(tf.logging.ERROR)
+
+
+DATA_DIR = '../datasets/minist/'
+
+# Load data 
+
+data_one_hot = read_data_sets(DATA_DIR, one_hot=True)
+
+data_classes = read_data_sets(DATA_DIR, one_hot=False)
+x_data, y_data = data_classes.train.images,data_classes.train.labels.astype(np.int32)
+x_test, y_test = data_classes.test.images,data_classes.test.labels.astype(np.int32)
+print(x_data)
+
+print('one-hot=False:\n')
+print(data_classes.train.labels)
+print('\n')
+print('one-hot=True:\n')
+print(data_one_hot.train.labels)
+
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=10)]
+
+dnn = learn.DNNClassifier(
+    feature_columns=feature_columns,
+    hidden_units=[200],
+    n_classes=10,
+    optimizer=tf.train.ProximalAdagradOptimizer(
+      learning_rate=0.2,
+    ))
+
+fit = dnn.fit(x=x_data,y=y_data, steps=2000,batch_size=128)
+print('Done fitting')
+
+ev = dnn.evaluate(x=x_test,y=y_test, steps=1)["accuracy"]
+print(ev)
+
+y_pred = dnn.predict(x=x_test,as_iterable=False)
+class_names = ['0','1','2','3','4','5','6','7','8','9']    
+    
+    
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+# Plot non-normalized confusion matrix
+plt.figure(figsize=[8,6])
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+"""
+"""
+learn
+from tensorflow.contrib import learn
+Estimators
+1. Instantiate the Estimator class
+model = learn.<Estimator>()
+2. Fit it using training data
+model.fit()
+3. Evaluate how good is the fit
+model.evaluate()
+4. Predict outcomes on new data
+model.predict()
+"""
 
 def unpickle(file):
     import cPickle
